e383a76f4a63b0006a6df2a9797ab08fe0beb0e7
imanol <imanolcorimayo@gmail.com>
2022-01-12 00:17:17 -0300

    controlled form to send data

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import styles from './styles/Add.module.css'
 
@@ -10,19 +10,30 @@ import { db } from '../firebase/index'
 
 export default function Add() {
 
+    const [form, setForm] = useState({
+        title: "",
+        type: "",
+        description: "",
+    })
+
     async function addTask(ev){
         ev.preventDefault()
         try {
             const docRef = await addDoc(collection(db, "tasks"), {
-                title: "Parcial 8465132 de álgebra 2",
-                type: "Facultad",
-                description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aspernatur totam consectetur eveniet, pariatur repudiandae commodi ipsa reiciendis dolor nihil est dolorem fugit quas. Perspiciatis libero similique animi ipsa fuga.",
+                ...form,
                 stateTask: "todo"
               });
               console.log("Document written with ID: ", docRef.id);
         } catch (e) {
             console.error("Error adding document: ", e)
         }
+    }
+
+    function handleForm(e) {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
     }
 
     return (
@@ -39,14 +50,35 @@ export default function Add() {
                 <div className={ styles.body }>
                     <div className="mb-3">
                         <label htmlFor="exampleFormControlInput1" className="form-label">Task title</label>
-                        <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Add title task" required/>
+                        <input 
+                        value={ form.title }
+                        name='title'
+                        onChange={ handleForm }
+                        type="text" 
+                        className="form-control" 
+                        id="exampleFormControlInput1" 
+                        placeholder="Add title task" required/>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="exampleFormControlTextarea1" className="form-label">Description</label>
-                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        <textarea 
+                        value={ form.description }
+                        onChange={ handleForm }
+                        name='description'
+                        className="form-control" 
+                        id="exampleFormControlTextarea1" 
+                        rows="3"></textarea>
                     </div>
                     <label htmlFor="exampleDataList" className="form-label">Type of task</label>
-                    <input className="form-control" list="datalistOptions" id="exampleDataList" placeholder="Select one..." required/>
+                    <input 
+                        onChange={ handleForm }
+                        value={form.type}
+                        name='type'
+                        className="form-control" 
+                        list="datalistOptions" 
+                        id="exampleDataList" 
+                        placeholder="Select one..." 
+                        required/>
                     <datalist id="datalistOptions">
                         <option value="Facultad"/>
                         <option value="Trabajo"/>
