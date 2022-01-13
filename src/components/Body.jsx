183d35660a64ceddc39b6f35498cedcfa546b8b5
imanol <imanolcorimayo@gmail.com>
2022-01-13 10:39:07 -0300

    get data from firebase

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,7 +6,19 @@ import styles from './styles/Body.module.css'
 import Filters from './Filters'
 import Cards from './Cards'
 
+// Firebase
+import { db } from '../firebase/index'
+import { getDocs, collection } from "firebase/firestore";
+
 export default function Body() {
+    async function getAllTasks(){
+        const tasksNow = await getDocs(collection(db, "tasks"))
+        tasksNow.forEach((doc) => {
+            console.log(doc.id, "=>", doc.data())
+        })
+    }
+
+    getAllTasks()
     return (
         <div className={ styles.divPrincipal }>
             <Filters className={ styles.filters }></Filters>
