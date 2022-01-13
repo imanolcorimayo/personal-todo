907349037deab5e28cc4de9ec69ac02f602d0710
imanol <imanolcorimayo@gmail.com>
2022-01-13 19:56:45 -0300

    add links to my social network

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -107,8 +107,17 @@ export default function Menu({ name, ...props }) {
 
             }
             <p>Know developer</p>
-            <button className='btn btn-outline-secondary'>Git Hub</button>
-            <button className='btn btn-outline-secondary'>LinkedIn</button>
+            {/* <a href="https://github.com/imanolcorimayo"> */}
+            <a className={ s.container_buttons_below_button } href="https://github.com/imanolcorimayo" target="_blank">
+              <div className='d-grid gap-2'>
+                <button className='btn btn-outline-secondary' onClick="https://github.com/imanolcorimayo">Git Hub</button>
+              </div>
+            </a>
+            <a className={ s.container_buttons_below_button } href="https://www.linkedin.com/in/imanolcorimayo/" target="_blank">
+              <div className='d-grid gap-2'>
+                <button className='btn btn-outline-secondary' onClick="https://github.com/imanolcorimayo">LinkedIn</button>
+              </div>
+            </a>
           </div>
           <>
             <Modal show={show} onHide={handleClose} animation={false}>
