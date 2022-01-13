a0f2f816b9bf3aa914a839043299f6c9001183dd
imanol <imanolcorimayo@gmail.com>
2022-01-13 18:34:43 -0300

    add profile on batTitle

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,7 @@ import Menu from "../components/Menu";
 
 import { useSelector, useDispatch } from 'react-redux'
 import { getTasks, showTodo } from "../redux/actions";
+import { useGlobalStorage } from "../hook/useGlobalStorage";
 
 
 
@@ -17,6 +18,9 @@ export default function Home() {
 
     const { type, tasks } = useSelector(state => state)
     const dispatch = useDispatch()
+
+    const [user,] = useGlobalStorage("user", "")
+    console.log(user.photo)
     
     useEffect(() => {
         dispatch(showTodo())
@@ -31,6 +35,15 @@ export default function Home() {
     return (
         <div className={styles.divPrincipal}>
             <div className={styles.barTitle}>
+                {user ?
+                    <div className={styles.barTitle_profile}>
+                        <img src={user.photo} alt="img not found" />
+                        <span> {user.name}</span>
+                    </div> :
+                    <>
+                        <span>Sign in on menu</span>
+                    </>
+                }
                 <h1 className={styles.h1}>{type === "todo" ? "To Do" :
                     type === "doing" ? "Doing" : "Done"}</h1>
                 <Menu placement={"end"} name={"end"}></Menu>
